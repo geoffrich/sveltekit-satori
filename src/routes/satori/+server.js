7a2cbcf455630d930b0d4eea1672051f6f3eb153
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-10-14 08:08:32 -0700

    Memo font path and cleanup

--- src/routes/satori/+server.js
+++ src/routes/satori/+server.js
@@ -5,34 +5,26 @@ import { Resvg } from '@resvg/resvg-js';
 import { html as toReactNode } from 'satori-html';
 import Image from '../../lib/Dots.svelte';
 import { parseQuery } from '$lib/parse';
+
+// this import won't copy the asset without an import on the client
 import sourceSerifPro from '$lib/fonts/SourceSerifPro-Regular.ttf';
 
-// TODO: better way to load fonts? can they be imported?
-// https://vercel.com/docs/concepts/functions/edge-functions/og-image-examples#using-a-custom-font
-
-/** @type {Record<string, Buffer>} */
+/** @type {Record<string, ArrayBuffer>} */
 const fontMemo = {};
 
 /**
- * @returns {Promise<Buffer | ArrayBuffer>}
+ * @param {string} path
+ * @returns {Promise<ArrayBuffer>}
  */
 async function getFont(path) {
-	console.log(path);
-	return fetch(path).then((r) => r.arrayBuffer());
-
-	const fontPath = join('.', sourceSerifPro);
-	if (fontMemo[fontPath]) {
-		return fontMemo[fontPath];
-	}
-	const fontData = await fs.readFile(fontPath);
-	fontMemo[fontPath] = fontData;
-	return fontData;
+	if (fontMemo[path]) return fontMemo[path];
+	const result = await fetch(path).then((r) => r.arrayBuffer());
+	fontMemo[path] = result;
+	return result;
 }
 
 /** @type {import('./$types').RequestHandler} */
 export const GET = async ({ url }) => {
-	console.log(url.origin);
-	console.log(sourceSerifPro);
 	const query = url.searchParams;
 	const { seed, width, height } = parseQuery(query);
 	const result = Image.render({ seed, width, height, satori: true });
