07859289da09fd7512ff452fdffe0ab13b1d41bc
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2024-01-21 15:22:59 -0800

    Use `read` to get font data

--- vite.config.js
+++ vite.config.js
@@ -3,27 +3,7 @@ import fs from 'fs';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit(), rawFonts(['.ttf'])],
-	optimizeDeps: {
-		exclude: ['@vercel/og']
-	}
+	plugins: [sveltekit()]
 };
-
-// adapted from mattjennings
-// https://github.com/mattjennings/mattjennings.io/blob/master/vite.config.js
-function rawFonts(ext) {
-	return {
-		name: 'vite-plugin-raw-fonts',
-		resolveId(id) {
-			return ext.some((e) => id.endsWith(e)) ? id : null;
-		},
-		transform(code, id) {
-			if (ext.some((e) => id.endsWith(e))) {
-				const buffer = fs.readFileSync(id);
-				return { code: `export default ${JSON.stringify(buffer)}`, map: null };
-			}
-		}
-	};
-}
 
 export default config;
